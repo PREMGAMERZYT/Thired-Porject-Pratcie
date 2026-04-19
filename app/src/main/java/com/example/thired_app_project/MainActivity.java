aeae3594200d0047db10426bed22b75c520e246b
PremCoder <kasturawwashunnamani@gmail.com>
2026-04-19 10:12:21 +0530

    added the toast to the button5

--- app/src/main/java/com/example/thired_app_project/MainActivity.java
+++ app/src/main/java/com/example/thired_app_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thired_app_project;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,19 +12,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button button = findViewById(R.id.button5);
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        button = findViewById(R.id.button5);
         //dp = dencity independent pixel is used to make the size same in every screen ratio
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Toast.makeText(MainActivity.this, "PremKumar", Toast.LENGTH_SHORT).show();
             }
         });
 
